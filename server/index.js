dc9b92a4daaa549b3a7e4bdc93c90bf9b06250c1
Ocel23 <tadeasocelkabusiness@gmail.com>
2023-10-21 21:08:31 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.set("trust proxy", 1);
 app.use(expressSession({
     secret: "a/#$sd#0$",
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     cookie: {
         secure: process.env.NODE_ENV === "production",
         httpOnly: true,
